b8a57339c08e71d8d344089e710f8888ce6486d0
yadavjj35-web <yadavjj35@gmail.com>
2026-08-03 23:56:28 +0530

    Add Tailwind CSS configuration file

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0ea5a4',
+        accent: '#7c3aed'
+      }
+    }
+  },
+  plugins: []
+};
