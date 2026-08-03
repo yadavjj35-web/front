f0aa179a5d4a50d242078df8c7ffca6f3195482a
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 00:04:23 +0530

    Initialize main.jsx with React application structure

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { ToastProvider } from './components/Toast/ToastProvider';
+import { ThemeProvider } from './contexts/ThemeContext';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
