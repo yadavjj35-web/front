2630e0a288088809e9e7eb2956ed9fccf21aabb0
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 09:39:03 +0530

    Add CloseIcon component with SVG markup

---
+++ src/icons/CloseIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CloseIcon(props) {
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" {...props}>
+      <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
