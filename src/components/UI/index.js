8600193f8a2d8d83a857da4c889e8cb6d2326bd3
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:40:33 +0530

    Add index.js for UI component exports

---
+++ src/components/UI/index.js
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { Skeleton } from './Skeleton';
+export { default as FileUpload } from './FileUpload';
+export { default as Avatar } from './Avatar';
+export { default as LoadingSpinner } from '../LoadingSpinner';
