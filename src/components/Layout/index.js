e0fceda721b39cf473091a2d283502179ef5a7e6
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:39:26 +0530

    Export layout components from index.js

---
+++ src/components/Layout/index.js
@@ -0,0 +1,4 @@
+export { default as Layout } from './Layout';
+export { default as Sidebar } from './Sidebar';
+export { default as Topbar } from './Topbar';
+export { default as MobileSidebar } from './MobileSidebar';
