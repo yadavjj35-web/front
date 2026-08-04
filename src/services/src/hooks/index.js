599fe82eaab942f5fdd395a5330fe410a0b7e48e
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:51:47 +0530

    Add index.js to export custom hooks

---
+++ src/services/src/hooks/index.js
@@ -0,0 +1,6 @@
+export { default as useFetch } from './useFetch';
+export { default as useDebounce } from './useDebounce';
+export { default as usePagination } from './usePagination';
+export { default as useInfiniteScroll } from './useInfiniteScroll';
+export { default as useKeyboardShortcuts } from './useKeyboardShortcuts';
+export { default as useAudioRecorder } from './useAudioRecorder';
