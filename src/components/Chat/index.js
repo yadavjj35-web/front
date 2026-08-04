9d1601c747568e2b0eb91fdac80ec7d7b0fe4e94
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:41:02 +0530

    Export Chat components from index.js

---
+++ src/components/Chat/index.js
@@ -0,0 +1,4 @@
+export { default as Conversation } from './Conversation';
+export { default as MessageList } from './MessageList';
+export { default as MessageInput } from './MessageInput';
+export { default as TypingIndicator } from './TypingIndicator';
