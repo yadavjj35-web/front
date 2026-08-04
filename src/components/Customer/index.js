0624e0e4bf9b39762d030d02942f219a1fa58bc3
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 12:54:50 +0530

    Export customer-related components from index.js

---
+++ src/components/Customer/index.js
@@ -0,0 +1,3 @@
+export { default as CustomerNotes } from './CustomerNotes';
+export { default as CustomerOrdersList } from './CustomerOrdersList';
+export { default as CustomerMessages } from './CustomerMessages';
