f4f2e41c17943091a0af2b6e542fc1097930e67b
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 13:26:32 +0530

    Add SupportIndex component with TicketForm

---
+++ src/pages/SupportIndex.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TicketForm from '../components/Support/TicketForm';
+
+export default function SupportIndex() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-3">Support</h2>
+      <TicketForm />
+    </div>
+  );
+}
