99e6ff357288e2c3615104e88cfd836760dbb14d
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:38:54 +0530

    Create OwnerApprovalCenter component

---
+++ src/pages/OwnerApprovalCenter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import OwnerApprovalList from '../components/OwnerApproval/OwnerApprovalList';
+
+export default function OwnerApprovalCenter() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-4">Owner Approval Center</h2>
+      <OwnerApprovalList />
+    </div>
+  );
+}
