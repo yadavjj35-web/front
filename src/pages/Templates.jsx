63308cef7945a2593fe6aa5c09c0edb5109c3bf0
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 11:33:21 +0530

    Add TemplatesPage component with TemplateManager

---
+++ src/pages/Templates.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TemplateManager from '../components/Templates/TemplateManager';
+
+export default function TemplatesPage() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-3">Templates</h2>
+      <TemplateManager />
+    </div>
+  );
+}
