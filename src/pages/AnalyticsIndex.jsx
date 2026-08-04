d50eddac7727f5df301ea68f51b85caae577f856
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 12:58:18 +0530

    Create AnalyticsIndex component to render Analytics

---
+++ src/pages/AnalyticsIndex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Analytics from './Analytics';
+import AnalyticsDetail from './AnalyticsDetail';
+
+export default function AnalyticsIndex() {
+  return <Analytics />;
+}
