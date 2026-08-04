e90424b614fc7b1634f5f8515edcc96ffb506ced
yadavjj35-web <yadavjj35@gmail.com>
2026-08-04 13:20:25 +0530

    Create CustomerIndex component to display Customers

---
+++ src/pages/CustomerIndex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Customers from './Customers';
+import CustomerProfile from './CustomerProfile';
+
+export default function CustomerIndex() {
+  return <Customers />;
+}
