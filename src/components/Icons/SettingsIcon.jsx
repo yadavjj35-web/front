c66c86bb425fca31cefc7abc20d0c5bcc3af3dc5
yadavjj35-web <yadavjj35@gmail.com>
2026-08-06 00:41:55 +0530

    Add SettingsIcon component with SVG rendering

---
+++ src/components/Icons/SettingsIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SettingsIcon(props) {
+  return (
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" {...props}>
+      <path d="M12 15.5A3.5 3.5 0 1 0 12 8.5a3.5 3.5 0 0 0 0 7z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 1 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 1 1-4 0v-.09a1.65 1.65 0 0 0-1-1.51 1.65 1.65 0 0 0-1.82.33l-.06.06A2 2 0 1 1 2.4 16.88l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 1 1 0-4h.09a1.65 1.65 0 0 0 1.51-1 1.65 1.65 0 0 0-.33-1.82L4.21 2.4A2 2 0 1 1 7 5.23l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 1 1 4 0v.09c.04.54.37 1 .95 1.32.59.33 1.35.3 1.82-.33l.06-.06A2 2 0 1 1 21.6 7.12l-.06.06a1.65 1.65 0 0 0-.33 1.82V9c.58.06 1.12.37 1.51 1H21a2 2 0 1 1 0 4h-.09c-.39.63-.93.94-1.51 1z" stroke="currentColor" strokeWidth="0.6" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
